8086633114491eb918d40b0f641a25fe0195c64c
Alonzo A <alonzoa2015@gmail.com>
2021-05-28 17:01:49 -0700

    add basic data for the quiz and questions

---
+++ quiz/animals/data.js
@@ -0,0 +1,190 @@
+const question1 = {
+    id: 'q1',
+    title: 'Question 1',
+    description:
+    `If a visitor approaches, are they welcome to your island?`,
+    choices: [{
+        id: 'opennessHigh',
+        description: 'All are welcome and may stay as long as they please.',
+        result: `
+        
+    `,
+        openness: 10
+    }, {
+        id: 'opennessMidHigh',
+        description: `You can visit but don't stay long`,
+        result: `
+        
+    `,
+        openness: 7
+    }, {
+        id: 'opennessMidLow',
+        description: `I'm not sure I like visitors.`,
+        result: `
+        
+    `,
+        openness: 3
+    },
+    {
+        id: 'opennessLow',
+        description: `Don't come here. Ever.`,
+        result: `
+      
+  `,
+        openness: 1
+    }]
+};
+const question2 = {
+    id: 'q1',
+    title: 'Question 1',
+    description:
+  `If a visitor approaches, are they welcome to your island?`,
+    choices: [{
+        id: 'opennessHigh',
+        description: 'All are welcome and may stay as long as they please.',
+        result: `
+      
+  `,
+        openness: 10
+    }, {
+        id: 'opennessMidHigh',
+        description: `You can visit but don't stay long`,
+        result: `
+      
+  `,
+        openness: 7
+    }, {
+        id: 'opennessMidLow',
+        description: `I'm not sure I like visitors.`,
+        result: `
+      
+  `,
+        openness: 3
+    },
+    {
+        id: 'opennessLow',
+        description: `Don't come here. Ever.`,
+        result: `
+    
+`,
+        openness: 1
+    }]
+};
+const question3 = {
+    id: 'q1',
+    title: 'Question 1',
+    description:
+  `If a visitor approaches, are they welcome to your island?`,
+    choices: [{
+        id: 'opennessHigh',
+        description: 'All are welcome and may stay as long as they please.',
+        result: `
+      
+  `,
+        openness: 10
+    }, {
+        id: 'opennessMidHigh',
+        description: `You can visit but don't stay long`,
+        result: `
+      
+  `,
+        openness: 7
+    }, {
+        id: 'opennessMidLow',
+        description: `I'm not sure I like visitors.`,
+        result: `
+      
+  `,
+        openness: 3
+    },
+    {
+        id: 'opennessLow',
+        description: `Don't come here. Ever.`,
+        result: `
+    
+`,
+        openness: 1
+    }]
+};
+const question4 = {
+    id: 'q1',
+    title: 'Question 1',
+    description:
+  `If a visitor approaches, are they welcome to your island?`,
+    choices: [{
+        id: 'opennessHigh',
+        description: 'All are welcome and may stay as long as they please.',
+        result: `
+      
+  `,
+        openness: 10
+    }, {
+        id: 'opennessMidHigh',
+        description: `You can visit but don't stay long`,
+        result: `
+      
+  `,
+        openness: 7
+    }, {
+        id: 'opennessMidLow',
+        description: `I'm not sure I like visitors.`,
+        result: `
+      
+  `,
+        openness: 3
+    },
+    {
+        id: 'opennessLow',
+        description: `Don't come here. Ever.`,
+        result: `
+    
+`,
+        openness: 1
+    }]
+};
+const question5 = {
+    id: 'q1',
+    title: 'Question 1',
+    description:
+  `If a visitor approaches, are they welcome to your island?`,
+    choices: [{
+        id: 'opennessHigh',
+        description: 'All are welcome and may stay as long as they please.',
+        result: `
+      
+  `,
+        openness: 10
+    }, {
+        id: 'opennessMidHigh',
+        description: `You can visit but don't stay long`,
+        result: `
+      
+  `,
+        openness: 7
+    }, {
+        id: 'opennessMidLow',
+        description: `I'm not sure I like visitors.`,
+        result: `
+      
+  `,
+        openness: 3
+    },
+    {
+        id: 'opennessLow',
+        description: `Don't come here. Ever.`,
+        result: `
+    
+`,
+        openness: 1
+    }]
+};
+
+const animalQuestions = [
+    question1,
+    question2,
+    question3,
+    question4,
+    question5
+];
+
+export default animalQuestions;
