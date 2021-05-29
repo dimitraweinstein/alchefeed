ee51e85ee1fad6b1322ffdc72c82e63c2bbd4298
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-28 17:33:59 -0700

    created all beginning local storage functions

---
+++ utils/local-Storage-utils.js
@@ -0,0 +1,37 @@
+
+
+const USER = 'USER';
+
+export function getUser() {
+    const stringyUser = localStorage.getItem(USER);
+
+    if (!stringyUser) return {};
+
+    const user = JSON.parse(stringyUser);
+
+    return user;
+}
+
+export function setUser(user) {
+    const stringyUser = JSON.stringify(user);
+
+    localStorage.setItem(USER, stringyUser);
+}
+
+export function completeQuiz(quizId) {
+    const user = getUser();
+
+    user.completed[quizId] = true;
+
+    setUser();
+}
+
+export function userHasCompletedAllQuizzes() {
+    const user = getUser();
+
+    for (let quiz of quizzes) {
+        if (!user.completed[quiz.id]) return false;
+    }
+    return true;
+}    
+
