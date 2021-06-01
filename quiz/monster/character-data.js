22a352456f9503a873c0c25b52a89e760928e7a8
Brianna Bias <brianna.bias@gmail.com>
2021-06-01 13:25:22 -0700

    monster fixes

--- quiz/monster/character-data.js
+++ quiz/monster/character-data.js
@@ -1,42 +1,40 @@
-export default [
+export const monsterFlavors = [{
 
-    {
-        name: 'rosaMonster',
+    rosaMonster: {
         id: 1,
         image: 'assets/monster/pink.png',
         description: `You're the right combination of nice and shady. A crisp floral flavor with zero sugar loaded with energy. You make throwing shade sound like something cute. Good for you!`,
         openness: 10
     },
 
-    {
-        name: 'whiteMonster',
+    whiteMonster: {
+
         id: 2,
         image: 'assets/monster/white.jpg',
         description: `You're just here to have a little fun and get things done! If you didn't have time to wait in line at Dutch Bros. for some cold brew, your go to is this baby right here.`,
         openness: 7
     },
 
-    {
-        name: 'zeroMonster',
+    zeroMonster: {
+
         id: 3,
         image: 'assets/monster/black.jpg',
         description: `You're a no frills type of person. You don't want to consume massive amounts of sugar but you still want to get jacked up. You're just trying to do you.`,
         openness: 5
     },
 
-    {
-        name: 'javaMonster',
+    javaMonster: {
+
         id: 4,
         image: 'assets/monster/green .jpg',
         description: `You're trying, you really are. But just like this flavor, you're not fooling anyone. Keep trying, but like, maybe a little better.`,
         openness: 3,
     },
 
-    {
-        name: 'ogMonster',
+    ogMonster: {
         id: 5,
         image: 'assets/monster/green.jpg',
         description: `Just like the warning on the can: You are not recommended for children or pregnant women. Reevaluate your decisions. Please.`,
         openness: 1
     }
-];
+}];
