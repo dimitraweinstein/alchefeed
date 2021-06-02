9823c1e1963a14714e18e6d67cf38458163473ae
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-02 10:21:22 -0700

    added all animal data

--- quiz/animals/character-data.js
+++ quiz/animals/character-data.js
@@ -1,37 +1,37 @@
 export const animalCharacters = [{
     isabelle: {
-        openness: 5,
+        openness: 7,
         conscientiousness: 3,
         extraversion: 7,
         agreeableness: 10,
-        neuroticism: 1
+        neuroticism: 3
     },
     blathers: {
         openness: 3,
-        conscientiousness: 7,
+        conscientiousness: 5,
         extraversion: 10,
-        agreeableness: 5,
+        agreeableness: 7,
         neuroticism: 1
     },
     kkSlider: {
         openness: 10,
-        conscientiousness: 5,
+        conscientiousness: 1,
         extraversion: 3,
-        agreeableness: 7,
-        neuroticism: 1
+        agreeableness: 3,
+        neuroticism: 7
     },
     redd: {
         openness: 1,
         conscientiousness: 7,
-        extraversion: 5,
-        agreeableness: 3,
+        extraversion: 1,
+        agreeableness: 1,
         neuroticism: 10
     },
     tom: {
         openness: 5,
         conscientiousness: 10,
-        extraversion: 7,
-        agreeableness: 3,
-        neuroticism: 1
+        extraversion: 5,
+        agreeableness: 5,
+        neuroticism: 5
     }
 }];
