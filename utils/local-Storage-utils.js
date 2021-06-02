213819965493ab25b98c141dba6ebaedecbcbc1e
Alonzo A <alonzoa2015@gmail.com>
2021-06-02 10:21:45 -0700

    added a few changes to LS

--- utils/local-Storage-utils.js
+++ utils/local-Storage-utils.js
@@ -1,3 +1,15 @@
+import aniamls from '../quiz/animals/data.js';
+import lebowski from '../quiz/lebowski/data.js';
+import monster from '../quiz/monster/data.js';
+import { findById } from './utils.js';
+const quizzes = [
+    lebowski,
+    aniamls,
+    monster
+];
+
+//need render header function
+
 
 
 const USER = 'USER';
@@ -5,7 +17,7 @@ const USER = 'USER';
 export function getUser() {
     const stringyUser = localStorage.getItem(USER);
 
-    if (!stringyUser) return {};
+    if (!stringyUser) return [];
 
     const user = JSON.parse(stringyUser);
 
@@ -26,12 +38,26 @@ export function completeQuiz(quizId) {
     setUser();
 }
 
-export function userHasCompletedAllQuizzes() {
+export function userFinishedQuiz(quiz, result){ //result is the final calc of who you got
     const user = getUser();
+    const newResult = { //gets result of who you got and adds to an obj
+        quiz: quiz.id,
+        result: result
+    };
+    user.push(newResult); //pushes result to user array
+    setUser(user);
+}
 
-    for (let quiz of quizzes) {
-        if (!user.completed[quiz.id]) return false;
+export function changeOpenness(userChoice){ //changes oppenness in a user obj in the array
+    const user = getUser();
+    const newUser = {
+        openness: 0
+    };
+    if (user.openness){ //sees if user.openness is created if not the user newUser to create it.
+        user.openness = user.openness + userChoice;
+    } else {
+        user.push(newUser);
     }
-    return true;
-}    
 
+    setUser(user);
+}
